8400c58e89c94326aeb9901ae71925fde6d8c112
Kyle Finley <kyle@finley.sh>
2017-11-12 20:44:23 -0800

    removed unused modules

--- Website/src/index.js
+++ Website/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import ProfilePaper from './containers/profile_paper';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
 ReactDOM.render(
   <MuiThemeProvider>
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider>
       <ProfilePaper />
     </Provider>
   </MuiThemeProvider>
